42824cc186252152eeb918bb9098a09b38de737c
Nicolas Ribeyrolle <ribeyr_n@epita.fr>
2017-12-02 03:49:01 +0100

    SDL: adding some other movement

--- src/sdl/sdl.c
+++ src/sdl/sdl.c
@@ -9,10 +9,11 @@ int main(int argc, char* argv[])
     bool quit = false;
     SDL_Event e;
     int height_rect = SCREEN_HEIGHT / 4;
-    //    w->texture = loadTexture("../ressource/texture.png");
+    w->texture = loadTexture("../ressource/texture.png");
     while(!quit)
     {
         SDL_PollEvent(&e);
+        SDL_Delay(50);
         if(e.type == SDL_KEYDOWN)
         {
             switch(e.key.keysym.sym)
@@ -21,10 +22,10 @@ int main(int argc, char* argv[])
                     quit = true;
                     break;
                 case SDLK_DOWN:
-                    height_rect--;
+                    height_rect++;
                     break;
                 case SDLK_UP:
-                    height_rect++;
+                    height_rect--;
                     break;
                 default:
                     continue;
@@ -34,16 +35,24 @@ int main(int argc, char* argv[])
         SDL_SetRenderDrawColor( w->renderer, 0xFF, 0xFF, 0xFF, 0xF);
         SDL_RenderClear( w->renderer );
 
-        SDL_Rect fillRect = { SCREEN_WIDTH / 4, height_rect,
+/*        SDL_Rect fillRect = { SCREEN_WIDTH / 4, height_rect,
             SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
         SDL_SetRenderDrawColor( w->renderer, 0xFF, 0x00, 0x00, 0xFF );
         SDL_RenderFillRect( w->renderer, &fillRect );
+        SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, 
+            SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3 };
+        SDL_SetRenderDrawColor( w->renderer, 0x00, 0xFF, 0x00, 0xFF );        
+        SDL_RenderDrawRect( w->renderer, &outlineRect );*/
+
+        SDL_Rect topLeftViewport;
+        topLeftViewport.x = 0;
+        topLeftViewport.y = height_rect;
+        topLeftViewport.w = SCREEN_WIDTH / 4;
+        topLeftViewport.h = SCREEN_HEIGHT / 4;
+        SDL_RenderSetViewport( w->renderer, &topLeftViewport );
+        SDL_RenderCopy( w->renderer, w->texture, NULL, NULL );
 
         SDL_RenderPresent(w->renderer);
-
-        /*SDL_RenderClear(w->renderer);
-          SDL_RenderCopy(w->renderer,w->texture,NULL,NULL);
-          SDL_RenderPresent(w->renderer);*/
     }
     close_sdl();
     return 0;
