a4b9906bbbb0c4e97d1783feb55aead3413c6514
Rod Guillaume <guilla_w@epita.fr>
2017-12-02 20:48:00 +0100

    GAME_LOOP: events function written, not tested

--- src/game_loop/events.c
+++ src/game_loop/events.c
@@ -1,29 +1,29 @@
-
 game_events(struct game_state *game_state)
 {
-   SDL_PollEvent(&e);
-   SDL_Delay(50);
-   if(e.type == SDL_KEYDOWN)
-   {
-       switch(e.key.keysym.sym)
-       {
-           case SDLK_q:
-               quit = true;
-               break;
-           case SDLK_r:
-               // RESTART
-               break;
-           case SDLK_RIGHT:
-               game_state->player->dir = RIGHT;
-               break;
-           case SDLK_LEFT:
-               game_state->player->dir = LEFT;
-               break;
-           case SDLK_UP:
-               game_state->player->dir = UP;
-               break;
-           default:
-               continue;
-       }
+    SDL_Event e;
+    SDL_PollEvent(&e);
+    SDL_Delay(30);
+    if(e.type == SDL_KEYDOWN)
+    {
+        switch(e.key.keysym.sym)
+        {
+        case SDLK_q:
+            game_state->quit = true;
+            break;
+        case SDLK_r:
+            game_state->restart = true;
+            break;
+        case SDLK_RIGHT:
+            game_state->player->dir = RIGHT;
+            break;
+        case SDLK_LEFT:
+            game_state->player->dir = LEFT;
+            break;
+        case SDLK_UP:
+            game_state->player->dir = UP;
+            break;
+        default:
+            continue;
+        }
    }
 }
