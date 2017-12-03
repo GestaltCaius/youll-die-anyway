d232d7b47250c9b307bd22516eaeaebf4fb6dbce
Rod Guillaume <guilla_w@epita.fr>
2017-12-03 22:52:02 +0100

    MISC: SDL module doxygen documentation

--- src/sdl/sdl.h
+++ src/sdl/sdl.h
@@ -1,16 +1,25 @@
 #ifndef SDL_H
 #define SDL_H
 
+/**
+** @file sdl.h
+** @brief 
+** @author Nicolas Ribeyrolle
+** @version 1.0
+** @date 2017-12-03
+*/
+
+
 #define SCREEN_WIDTH  1024
 #define SCREEN_HEIGHT  1024
 #define BLOCK_SIZE 32
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
-#include <stdio.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -25,11 +34,40 @@ enum color
 };
 
 
+/**
+** @brief SDL Window constructor
+*/
 void init_mywindow(struct game_state *gs);
+
+/**
+** @brief SDL Window destructor
+*/
 void close_sdl(struct game_state *gs);
+
+/**
+** @brief PNG file texture loader
+** @return 
+*/
 SDL_Texture* loadTexture(char* path,struct game_state *gs);
+
+/**
+** @brief Draw function for main game loop
+*/
 void game_window_draw(struct game_state *gs);
+
+/**
+** @brief SDL Window initializer
+*/
 bool init_window(struct game_state *gs);
+
+/**
+** @brief Main title window draw function
+*/
 void menu_window(struct game_state *gs);
+
+/**
+** @brief End title draw function
+*/
 void menu_end(struct game_state *gs);
+
 #endif
